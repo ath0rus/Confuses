b5571bb4a5dd277f036e1366aaacb2295c9fb213
ath0rus <65640442+ath0rus@users.noreply.github.com>
2020-10-28 14:00:41 +1000

    Update Bot2.py

--- Bot2.py
+++ Bot2.py
@@ -5,8 +5,8 @@ import random as rand
 import time
 import hashlib
 
-vchannels = ['1a', '2a', '💬-general', '🤖bot-commands-and-stuff']
-# vusers = ['ath0rus#2069', 'wallGraffiti#5365', 'phos#4938']
+vchannels = []
+# vusers = []
 
 cat_gif = ['https://tenor.com/view/kitty-highkitten-mdmacat-cat-happykitty-gif-6198981', 
 'https://tenor.com/view/cat-meow-big-lips-gif-13233291', 
@@ -17,13 +17,13 @@ cat_gif = ['https://tenor.com/view/kitty-highkitten-mdmacat-cat-happykitty-gif-6
 csoon = "That command is coming soon"
 
 my_det = '''
-Host OS: Windows 10 (2004)
-RAM: 12GB DDR4
-System Host IP: ||Nope||
-Host Manafatuer:Acer Laptop
-CPU: i5 8250U
-GPU: MX130 (deicated) or Intel HD Graphics
-Storage: 1TB samsung EVO SSD or LITEON 256GB SSD
+Host OS: 
+RAM: 
+System Host IP: 
+Host Manafatuer:
+CPU: 
+GPU: 
+Storage:
 The Bots Code is stored on Github.com
 '''
 
@@ -45,14 +45,8 @@ cmds = '''
 **say**: makes the bot say waht ever is put after the say command
 '''
 
-server_ids = {
-    #BT(oym)
-    "bt": 715493968921493614,
-    #Cordless Cat
-    "cc": 678543198846713861,
-    #Salt
-    "slt": 339355554084290560,
-    }
+server_ids = {}
+    
 
 #open the file where the token is stored
 o_token = open('token.txt', 'r')
@@ -109,16 +103,7 @@ async def cat(ctx):
 
 @bot.command()
 async def fate(ctx):
-        fs = [' Got Killed by a falling Minecraft Anvil', ' bled out from 28 stab wounds', ' Was eaten to bits by Chonky Kittens', ' Fell off a sky scraper', 
-        ' Stayed up for too long and died from sleep deprivation', ' Bled out from a DEEEEEP paper cut', ' Spent to much time watching Juicy and drank Paint', 
-        ' Decided to play Dumb Ways to Die IRL and never got saved', ' Trusted the imposter', ' Got voted off for calling an emergency meeting to say "Hi"',
-        ' Suffocated by a Fur ball', ' Miscalcualted a dive off a 80 foot (24.384m) cliff', ' Danced near molten lava and fell in', ' Picked a fight with Elsa',
-        ' Thought they ran the animal Kingdom', ' Ignored rairoad crossing signals', ' Took the vending machine challenge', ' Was a distracted driver', ' shopped on black friday', 
-        ' Forgot how to breathe', ' Drove off of a cliff but survive but die of shock from the high price of their hospital bill', ' Listened to Justin Beiber for Too Long',
-        ' Drowned in Jell-o (jelly)', ' Tryed to eat a whole Big Mac in one bite and chocked to death', ' Chocked on their own tongue (Actually possible)', ' Got their face caught in an egg beater',
-        ' Tryed to fly off a building', ' Drank too much anti-Freeze', ' Got their head ripped off by an elevator', ' got stuck in a tanning booth', ' Walked across quicksand while carrying an anvil',
-        ' set fire to their own hair', ' Tryed to catch a piranha with their own tongue', ' Got stabbed with a cucumber', ' Poked a stick at a grizzly bear', 
-        ' Their teacher Gave them ten tons of homework, and their brain exploded', ' Took their helmet off in outer space and their head explodes they froze to death', ' Played Russian Roulette with a fully loaded Uzi']
+        fs = [' Got Killed by a falling Minecraft Anvil', ' bled out from 28 stab wounds'] #the rest got removed so that they are special to the bot
         #num1 = rand.randint(1, len(fs) - 1)
         await ctx.send(f'{ctx.author.name}{rand.choice(fs)}')
 
@@ -129,10 +114,6 @@ async def only_poop(ctx):
 @bot.command()
 async def say(ctx, *, sy1):
     await ctx.send(sy1)
-
-# @bot.command()
-# async def (╯°□°）╯︵ ┻━┻ (ctx):
-#     await ctx.send('"That is Illegal Sir" - Hollis\n/unflip')
 
 @bot.command()
 async def emoji(ctx):
